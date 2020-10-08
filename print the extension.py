71e8207cbca8fc825bc2c4e38e84e7904f69d7c0
avatar369 <72565537+avatar369@users.noreply.github.com>
2020-10-08 20:41:10 +0530

    MyCaptain first Task

---
+++ print the extension.py
@@ -0,0 +1,7 @@
+E = input("Input the Filename: ")
+e = E.split('.')[1]
+if e == 'py':
+    e = "'python'"
+    print ("The extension of the file is : ", e)
+else:
+    print ("The extension of the file is : ", e)
